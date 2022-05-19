9642a80cc70bec519e6ed33b72d82a95cb9f0b66
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-05-19 17:37:27 +0100

    Revert "🔒Secured KEY Access"
    This reverts commit bfeac6e065d20fcf1944d08345f3ccfaecfa00f4.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import ProductApp from "./components/Products";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAnzKq0fsXQndEnnn0W1SwEG8EuFqXewas",
+  authDomain: "shoppingwebapp-b6885.firebaseapp.com",
+  projectId: "shoppingwebapp-b6885",
+  storageBucket: "shoppingwebapp-b6885.appspot.com",
+  messagingSenderId: "304058608398",
+  appId: "1:304058608398:web:9613ebfdbd46369100055f",
 };
 // ....
 
